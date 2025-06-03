6e35cf7672ab02b2bde9d043b88849d052b2abde
AngelRagel05 <jimenezragelangel@gmail.com>
2025-06-03 15:51:07 +0200

    Agregar favicon a las vistas 'main', 'nosotros' y 'proyecto', y añadir archivo de imagen 'Logo.ico'

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,6 +8,9 @@
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('Logo.ico') }}" type="image/x-icon">
+
     <title>@yield('titulo')</title>
 </head>
 

d808bae159722578ae8944bf143c653041a699ea
AngelRagel05 <jimenezragelangel@gmail.com>
2025-06-06 12:59:56 +0200

    Refactor code structure for improved readability and maintainability

--- Ejercicio4/resources/views/layouts/_components/card.blade.php
+++ Ejercicio4/resources/views/layouts/_components/card.blade.php
@@ -1,4 +1,5 @@
 <div>
     <h3>{{ $title }}</h3>
+    <img src="{{ asset('assets/img/MrSuicideSheep_03.png')}}" alt="Ejemplo imagen" class="img-fluid" style="width: 20%; height: auto;">
     <p>{{ $content }}</p>
 </div>

5a5b5a65be73e56eac9bf624ad260755394acdb1
AngelRagel05 <jimenezragelangel@gmail.com>
2025-06-06 10:33:02 +0200

    Agregar menú de navegación y favicon a la vista de landing

--- Ejercicio4/resources/views/layouts/landing.blade.php
+++ Ejercicio4/resources/views/layouts/landing.blade.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap 5 CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('Logo.ico') }}" type="image/x-icon">
+
     <title>
         @yield('title', 'Mi sitio web')
     </title>
 </head>
+
 <body>
+    @include('layouts._partials.menu')
     @yield('content')
 </body>
+
 </html>

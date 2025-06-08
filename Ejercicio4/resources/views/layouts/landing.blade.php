67a3148d8ed293f3fc16e65042fefcc3efb3d1aa
AngelRagel05 <jimenezragelangel@gmail.com>
2025-06-09 00:52:07 +0200

    Agregar secciones para estilos y scripts en la plantilla de landing

--- Ejercicio4/resources/views/layouts/landing.blade.php
+++ Ejercicio4/resources/views/layouts/landing.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    @yield('styles')
+
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -20,6 +22,7 @@
 <body>
     @include('layouts._partials.menu')
     @yield('content')
+    @yield('scripts')
 </body>
 
 </html>

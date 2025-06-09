35e1cf5f5b392d4df3410eefd035c2286de80e26
AngelRagel05 <jimenezragelangel@gmail.com>
2025-06-09 10:30:41 +0200

    Agregar migraciones para la tabla de autores y actualizar la tabla de notas

--- Ejercicio5/database/migrations/2025_06_09_083016_create_authors_table.php
+++ Ejercicio5/database/migrations/2025_06_09_083016_create_authors_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('notes', function (Blueprint $table) {
-            $table->string('author', 255);
-            $table->dropColumn('deadline');
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropColumns('author');
+        Schema::dropIfExists('authors');
     }
 };

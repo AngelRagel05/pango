71a4339e9af30d8d4624a4f29b50f8dbcdaeafdc
AngelRagel05 <jimenezragelangel@gmail.com>
2025-06-09 11:42:50 +0200

    Agregar funcionalidad para listar usuarios en UserController y actualizar vista de usuarios

--- Ejercicio6/resources/views/user/index.blade.php
+++ Ejercicio6/resources/views/user/index.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Hello World</h1>
+    <h1>Users list:</h1>
+    @if ($users->isEmpty())
+        <p>the user list is empty</p>
+    @else
+        <ul>
+            @foreach ($users as $user)
+                <li>{{ $user->name }}</li>
+            @endforeach
+        </ul>
+    @endif
 </body>
+
 </html>
